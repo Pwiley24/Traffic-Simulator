d163c8b77017324c0fd130cb609d82ac191611b8
Pwiley24 <97415028+Pwiley24@users.noreply.github.com>
2022-04-30 09:45:21 -0700

    Update Sports.java

--- Sports.java
+++ Sports.java
@@ -4,17 +4,25 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Sports extends Vehicles{
+	
+	Image myImage;
 
 	public Sports(int newx, int newy) {
 		super(newx, newy);
 		width = 70;
 		height = 30;
 		speed = 15;
+		try{
+			myImage = ImageIO.read(new File("sportscar.png");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 	
 	public void paintMethod(Graphics g) {
-		g.setColor(Color.YELLOW);
-		g.fillRect(x, y, width, height);
+		/*g.setColor(Color.YELLOW);
+		g.fillRect(x, y, width, height);*/
+		g.drawImage(myImage, x, y, null);
 	}
 	
 }
